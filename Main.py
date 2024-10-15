724b9f275a3627e6a4ed1537360168c093e99bfa
TCJay_Append <123647533+TCBJYT@users.noreply.github.com>
2024-10-15 23:26:16 +0800

    Update
    Added assigning function.
    
    Already has embedding

--- Main.py
+++ Main.py
@@ -1,64 +1,45 @@
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama import OllamaEmbeddings
-# Create a vector store with a sample text
+# Vector import
 from langchain_core.vectorstores import InMemoryVectorStore
 
-Message_History = [
-{
-    'id': 1,
-    'prompt': 'What is your name',
-    'response': 'My name is fred the cat, known as a pet of the family of barreras, and the family of braga'
 
-}, 
-{
-    'id': 2,
-    'prompt': 'Where are you from',
-    'response': 'Land of the philippines, bulacan, san jose del monte'
-
-}, 
-{
-    'id': 3,
-    'prompt': 'Who is your owner',
-    'response': 'My owner is known as Jay Marc Braga Barreras'
-
-}
-]
-
+#embedding or something
 template = """
-Here is the conversation history: {context}
+Conversation history: {context}
+Name(User): {name}
 Question: {question}
+About you(The AI): {AI_introduction}
 """
 
-
-embeddings = OllamaEmbeddings(model="llama3")
 model = OllamaLLM(model="llama3")
 prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
-
+    
 def handle_conversation():
     context = ""
-    print("Welcome to the AI Bot!")
+    nameuser = input("What name would you like to be called?")
+    AI_introduction = input("Explain your AI: ")
+    print("Type in the AI on what you want: ")
     while True:
         user_input = input("you: ")
         if user_input.lower() == "exit":
             break
 
-        result = chain.invoke({"context": context, "question": user_input})
+        result = chain.invoke(
+                {
+                    "AI_introduction": AI_introduction, 
+                    "name": nameuser,
+                    "context": context, 
+                    "question": user_input
+                }
+            )
+        
         print("Bot: ", result)
+        
         context += f"\nUser: {user_input}\nAI: {result}"
 
-        vectorstore = InMemoryVectorStore.from_texts([Message_History],embedding=embeddings,)
 
-        # Use the vectorstore as a retriever
-        retriever = vectorstore.as_retriever()
-
-        # Retrieve the most similar text
-        retrieved_documents = retriever.invoke(user_input)
-
-        # show the retrieved document's content
-        retrieved_documents[0].page_content  
-
-   
 if __name__ == "__main__":
     handle_conversation()
